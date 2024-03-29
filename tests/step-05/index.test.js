7494b2ef633a85b82297f567d6e3c46793ba169b
Neeraj Meena <mneeraj2133@gmail.com>
2024-03-29 15:12:39 +0530

    step8 done

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -3,7 +3,7 @@ const parseQuery = require('../../src/queryParser');
 const executeSELECTQuery = require('../../src/index');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
@@ -11,18 +11,20 @@ test('Read CSV File', async () => {
 });
 
 test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM sample';
+    const query = 'SELECT id, name FROM student';
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        table: 'sample',
+        table: 'student',
+        "joinCondition": null,
+        "joinTable": null,
         whereClauses: [],
     });
 });
 
 
 test('Execute SQL Query', async () => {
-    const query = 'SELECT id, name FROM sample';
+    const query = 'SELECT id, name FROM student';
     const result = await executeSELECTQuery(query);
     expect(result.length).toBeGreaterThan(0);
     expect(result[0]).toHaveProperty('id');
@@ -32,11 +34,13 @@ test('Execute SQL Query', async () => {
 });
 
 test('Parse SQL Query with Multiple WHERE Clauses', () => {
-    const query = 'SELECT id, name FROM sample WHERE age = 30 AND name = John';
+    const query = 'SELECT id, name FROM student WHERE age = 30 AND name = John';
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        table: 'sample',
+        table: 'student',
+        "joinCondition": null,
+        "joinTable": null,
         whereClauses: [{
             "field": "age",
             "operator": "=",
@@ -50,7 +54,7 @@ test('Parse SQL Query with Multiple WHERE Clauses', () => {
 });
 
 test('Execute SQL Query with Multiple WHERE Clause', async () => {
-    const query = 'SELECT id, name FROM sample WHERE age = 30 AND name = John';
+    const query = 'SELECT id, name FROM student WHERE age = 30 AND name = John';
     const result = await executeSELECTQuery(query);
     expect(result.length).toBe(1);
     expect(result[0]).toEqual({ id: '1', name: 'John' });
